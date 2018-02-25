a2eb8615012e9cc622d22441bd9643d54a0a2628
Chris Scott <chris@chrisdjscott.co.uk>
2018-02-25 15:26:17 +1300

    Adding task for creating a new post

--- tasks.py
+++ tasks.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
-
+from __future__ import print_function
 import os
 import shutil
+import re
+import datetime
+import subprocess
 
 import invoke
 
@@ -28,9 +31,92 @@ def build(ctx):
         failures = builder.build_all()
 
     if failures:
-        raise invoke.Exit('Builder failed.')
+        print("Error: Builder failed!")
+        raise invoke.Exit(1)
 
     # copy redirect file for netlify
     redirect_input = os.path.join(ROOT_DIR, "redirects.txt")
     redirect_output = os.path.join(OUTPUT_DIR, '_redirects')
     shutil.copy(redirect_input, redirect_output)
+
+
+@invoke.task
+def newpost(ctx, title, slug=None, add_gpx=False, description=None, date=None):
+    def slugify(s):
+        s = s.lower()
+        for c in [' ', '-', '.', '/']:
+            s = s.replace(c, '_')
+        s = re.sub('\W', '', s)
+        s = s.replace('_', ' ')
+        s = re.sub('\s+', ' ', s)
+        s = s.strip()
+        s = s.replace(' ', '-')
+
+        return s
+
+    # slug for the post
+    post_slug = slug if slug is not None else slugify(title)
+
+    # check the branch and switch to a new one
+    #   1. do a status and fetch
+    invoke.run('git status')
+    invoke.run("git fetch")
+
+    #   2. check we are on master
+    git_status = subprocess.check_output('git status', universal_newlines=True, shell=True)
+    if 'On branch master' not in git_status:
+        print("Error: should be starting from master")
+        raise invoke.Exit(1)
+
+    #   3. check up to date
+    if 'Your branch is behind' in git_status:
+        print("Error: branch is behind")
+        raise invoke.Exit(1)
+
+    #   4. create new branch
+    invoke.run("git checkout -b %s" % post_slug)
+
+    # directory for the content
+    content_dir = os.path.join(ROOT_DIR, "content")
+    post_dir = os.path.join(content_dir, post_slug)
+    if os.path.exists(post_dir):
+        print("Error: post directory already exists: %s" % post_dir)
+        raise invoke.Exit(1)
+    os.mkdir(post_dir)
+
+    # content
+    content_dict = {
+        'title': title,
+        'today': datetime.datetime.today().strftime('%Y-%m-%d'),
+        'gpx_file': post_slug + '.gpx' if add_gpx else '',
+        'description': '' if description is None else description,
+        'date': '' if date is None else date,
+    }
+
+    # content file
+    content_file = os.path.join(post_dir, "contents.lr")
+    templ = """title: %(title)s
+---
+date: %(date)s
+---
+pub_date: %(today)s
+---
+author: Chris Scott
+---
+image:
+---
+latitude:
+---
+longitude:
+---
+gpx: %(gpx_file)s
+---
+description: %(description)s
+---
+body:
+
+""" % content_dict
+    with open(content_file, "w") as fh:
+        fh.write(templ)
+
+
