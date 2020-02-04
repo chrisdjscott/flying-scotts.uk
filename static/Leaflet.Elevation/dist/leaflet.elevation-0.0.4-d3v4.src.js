124a3ff86a87d3ae9f1e8650ede64afddd81e11f
Chris Scott <chris@chrisdjscott.co.uk>
2020-02-04 13:00:18 +1300

    Update other dependencies

--- static/Leaflet.Elevation/dist/leaflet.elevation-0.0.4-d3v4.src.js
+++ static/Leaflet.Elevation/dist/leaflet.elevation-0.0.4-d3v4.src.js
@@ -11,7 +11,7 @@ L.Control.Elevation = L.Control.extend({
             left: 60
         },
         useHeightIndicator: true,
-        interpolation: "linear",
+        interpolation: d3.curveLinear,
         hoverNumber: {
             decimalsX: 3,
             decimalsY: 0,
@@ -45,14 +45,14 @@ L.Control.Elevation = L.Control.extend({
         opts.yTicks = opts.yTicks || Math.round(this._height() / 30);
         opts.hoverNumber.formatter = opts.hoverNumber.formatter || this._formatter;
 
-        var x = this._x = d3.scale.linear()
+        var x = this._x = d3.scaleLinear()
             .range([0, this._width()]);
 
-        var y = this._y = d3.scale.linear()
+        var y = this._y = d3.scaleLinear()
             .range([this._height(), 0]);
 
-        var area = this._area = d3.svg.area()
-            .interpolate(opts.interpolation)
+        var area = this._area = d3.area()
+            .curve(opts.interpolation)
             .x(function(d) {
                 var xDiagCoord = x(d.dist);
                 d.xDiagCoord = xDiagCoord;
@@ -77,7 +77,7 @@ L.Control.Elevation = L.Control.extend({
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var line = d3.svg.line();
+        var line = d3.line();
         line = line
             .x(function(d) {
                 return d3.mouse(svg.select("g"))[0];
@@ -98,7 +98,7 @@ L.Control.Elevation = L.Control.extend({
             .style("stroke", "none")
             .style("pointer-events", "all");
 
-        if (L.Browser.mobile) {
+        if (L.Browser.touch) {
 
             background.on("touchmove.drag", this._dragHandler.bind(this)).
             on("touchstart.drag", this._dragStartHandler.bind(this)).
@@ -287,7 +287,7 @@ L.Control.Elevation = L.Control.extend({
         //Makes this work on IE10 Touch devices by stopping it from firing a mouseout event when the touch is released
         container.setAttribute('aria-haspopup', true);
 
-        if (!L.Browser.mobile) {
+        if (!L.Browser.touch) {
             L.DomEvent
                 .disableClickPropagation(container);
             //.disableScrollPropagation(container);
@@ -308,7 +308,7 @@ L.Control.Elevation = L.Control.extend({
             link.href = '#';
             link.title = this.options.controlButton.title;
 
-            if (L.Browser.mobile) {
+            if (L.Browser.touch) {
                 L.DomEvent
                     .on(link, 'click', L.DomEvent.stop)
                     .on(link, 'click', this._expand, this);
@@ -365,10 +365,9 @@ L.Control.Elevation = L.Control.extend({
 
         if (opts.imperial) {
             y.attr("class", "y axis")
-                .call(d3.svg.axis()
+                .call(d3.axisLeft()
                     .scale(this._y)
-                    .ticks(this.options.yTicks)
-                    .orient("left"))
+                    .ticks(this.options.yTicks))
                 .append("text")
                 .attr("x", -37)
                 .attr("y", 3)
@@ -376,10 +375,9 @@ L.Control.Elevation = L.Control.extend({
                 .text("ft");
         } else {
             y.attr("class", "y axis")
-                .call(d3.svg.axis()
+                .call(d3.axisLeft()
                     .scale(this._y)
-                    .ticks(this.options.yTicks)
-                    .orient("left"))
+                    .ticks(this.options.yTicks))
                 .append("text")
                 .attr("x", -45)
                 .attr("y", 3)
@@ -394,10 +392,9 @@ L.Control.Elevation = L.Control.extend({
         if (opts.imperial) {
             x.attr("class", "x axis")
                 .attr("transform", "translate(0," + this._height() + ")")
-                .call(d3.svg.axis()
+                .call(d3.axisBottom()
                     .scale(this._x)
-                    .ticks(this.options.xTicks)
-                    .orient("bottom"))
+                    .ticks(this.options.xTicks))
                 .append("text")
                 .attr("x", this._width() + 10)
                 .attr("y", 15)
@@ -406,10 +403,9 @@ L.Control.Elevation = L.Control.extend({
         } else {
             x.attr("class", "x axis")
                 .attr("transform", "translate(0," + this._height() + ")")
-                .call(d3.svg.axis()
+                .call(d3.axisBottom()
                     .scale(this._x)
-                    .ticks(this.options.xTicks)
-                    .orient("bottom"))
+                    .ticks(this.options.xTicks))
                 .append("text")
                 .attr("x", this._width() + 20)
                 .attr("y", 15)
